3f634d4b81226c5c651ff7317be7f87ac974a2ec
abdalrzag eisa <bmjfdrh@gmail.com>
2024-03-09 04:07:14 +0300

    resize image

--- src/main.c
+++ src/main.c
@@ -1,19 +1,21 @@
 #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
 
 #include <stdio.h>
 #include <string.h>
 
 #include "include/stb_image.h"
+#include "include/stb_image_resize.h"
 
 #define MAX_IMAGES 100
 
 int main(int argc, char *argv[])
 {
-    char chars[] = "^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-    int chars_len = strlen(chars);
+    int new_width = 80;
+    int new_height = 80;
 
-    int desired_width = 80;
-    int desired_height;
+    char chars[] = ".:-=+*#%@";
+    int chars_len = strlen(chars);
 
     char *images[MAX_IMAGES];
     int img_idx = 0;
@@ -37,26 +39,26 @@ int main(int argc, char *argv[])
         char *image = images[i];
         int width, height, channels;
 
-        unsigned char *image_data = stbi_load(image, &width, &height, &channels, 3);
+        unsigned char *pixels = stbi_load(image, &width, &height, &channels, 3);
 
-        if (image_data == NULL)
+        if (pixels == NULL)
         {
             printf("Failed to load image: %s", image);
-            stbi_image_free(image_data);
+            stbi_image_free(pixels);
             continue;
         }
 
-        /* printf("Loaded image with a width of %dpx, a height of %dpx, and %d channels\n", width, height, channels); */
+        unsigned char *resized_pixels = (unsigned char *)malloc(new_width * new_height * channels);
 
-        unsigned char *pixels = image_data;
+        stbir_resize_uint8(pixels, width, height, 0, resized_pixels, new_width, new_height, 0, channels);
 
-        for (int row_idx = 0; row_idx < height; row_idx++)
+        for (int row_idx = 0; row_idx < new_height; row_idx++)
         {
-            for (int col_idx = 0; col_idx < width; col_idx++)
+            for (int col_idx = 0; col_idx < new_width; col_idx++)
             {
-                unsigned char red = *pixels++;
-                unsigned char green = *pixels++;
-                unsigned char blue = *pixels++;
+                unsigned char red = *resized_pixels++;
+                unsigned char green = *resized_pixels++;
+                unsigned char blue = *resized_pixels++;
 
                 float average = (red + green + blue) / 3.0f;
                 int char_idx = (int)(chars_len * (average / 255.0f));
@@ -66,7 +68,7 @@ int main(int argc, char *argv[])
             putchar('\n');
         }
 
-        stbi_image_free(image_data);
+        stbi_image_free(pixels);
     }
 
     return 0;
